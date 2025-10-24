da470f01af4574a2be633ae0ae7d7c3bbb02a8e6
Favour Mfon <favourmichael004@gmail.com>
2025-10-24 16:17:22 +0100

    Add new changes to the emailservice

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -12,8 +12,9 @@ export class EmailService {
       
         this.transporter = nodemailer.createTransport({
             service: 'gmail',
-            port: 465,
-            secure: true,
+            port: 587,
+            secure: false,
+            requireTLS: true,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASSWORD
