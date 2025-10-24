e183275aa966dd94a8966fa6943510180efa9dcc
Favour Mfon <favourmichael004@gmail.com>
2025-10-24 18:28:02 +0100

    Add resend Api to emailservices, remove nodemailer

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -3,49 +3,32 @@ import { Injectable } from "@nestjs/common";
 import * as nodemailer from "nodemailer"; // Use import * as
 import { SendMailOptions, Transporter } from "nodemailer"; // Import Transporter and SendMailOptions
 import { sendEmailDto } from "./dto/email.dto";
+import { Resend } from "resend";
 
 @Injectable()
 export class EmailService {
-    private transporter: Transporter;
+    private resend: Resend
 
     constructor() {
       
-        this.transporter = nodemailer.createTransport({
-            service: 'smtp.gmail.com',
-            port: 587,
-            secure: false,
-            requireTLS: true,
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASSWORD
-            },
-            connectionTimeout: 10000
-        });
+        this.resend = new Resend(process.env.RESEND_API_KEY);
     }
 
     async sendEmail(dto: sendEmailDto): Promise<boolean> {
         const { recipients, subject, html } = dto;
-
-
-        const mailOptions: SendMailOptions = { 
-            from: process.env.EMAIL_USER,
-            to: recipients,
-            subject: subject,
-            html: html
-        };
-
         try {
-            const info = await this.transporter.sendMail(mailOptions); 
-            console.log('Message sent: %s', info.messageId);
-            return true
+            const response = await this.resend.emails.send({
+                from: process.env.EMAIL_USER!, 
+                to: recipients,
+                subject: subject,
+                html: html,
+            });
+
+            console.log('Message sent via Resend. ID: %s', response.data?.id);
+            return true;
         } catch(error: any) {
-            console.error('Email Send Failed - Nodemailer Error Code:', error.code); 
-            console.error('Email Send Failed - Nodemailer Error Message:', error.message);
-            console.error('Email Send Failed - Full Error Object:', error);
-            if (error.response) {
-            console.error('Nodemailer SMTP Response:', error.response);
-            }
-            return false
+            console.error('Error sending mail via Resend: ', error);
+            return false;
         }
     }
 }
